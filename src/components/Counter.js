a6ec67a6e72374ae3540b8627c726ba8474d3af6
Guy Siedes <3strategy@gmail.com>
2023-08-23 09:24:08 +0300

    completed ph2 - adds 100 and removes 100

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,19 +1,24 @@
 import './Counter.css';
 import { useState } from "react";
 function Counter() {
-  console.log("a");
-  const [counter, setCounter] = useState(0);
+    console.log("a");
+    const [counter, setCounter] = useState(0);
 
-  function inc() {
-    setCounter(counter + 1);
-  }
-  return (
-    <div>
-      <h1>My Counter</h1>
-      <p>{counter}</p>
-      <button onClick={inc}>Click My</button>
-    </div>
-  )
+    function inc() {
+        setCounter(counter + 100);
+    }
+
+    function dec() {
+        setCounter(counter - 100);
+    }
+    return (
+        <div>
+            <h1>My Counter</h1>
+            <p>{counter}</p>
+            <button onClick={inc}>Add 100</button>
+            <button onClick={dec}>Subtract 100</button>
+        </div>
+    )
 }
 
 export default Counter;
